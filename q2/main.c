eb0ab2e0684f9d1946d8bba93fde6f2404e33c24
Sandeep Raj Kumbargeri <sandeep.kumbargeri@colorado.edu>
2018-04-08 23:04:33 -0600

    Add q2

---
+++ q2/main.c
@@ -0,0 +1,171 @@
+//*****************************************************************************
+// Author: Sandeep Raj Kumbargeri
+// Date: 6-April 2018
+//
+// hw5 q2 - Simple example for creation of two tasks to blink different LEDs at
+//          different frequencies in FreeRTOS using timer callback handler and
+//          semaphores.
+//
+// Copyright (c) 2013-2016 Texas Instruments Incorporated.  All rights reserved.
+// Software License Agreement
+//
+// Texas Instruments (TI) is supplying this software for use solely and
+// exclusively on TI's microcontroller products. The software is owned by
+// TI and/or its suppliers, and is protected under applicable copyright
+// laws. You may not combine this software with "viral" open-source
+// software in order to form a larger program.
+//
+// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
+// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
+// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
+// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
+// DAMAGES, FOR ANY REASON WHATSOEVER.
+//
+// This is part of revision 2.1.3.156 of the EK-TM4C1294XL Firmware Package.
+//
+//*****************************************************************************
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <strings.h>
+
+#include "FreeRTOS.h"
+#include "portmacro.h"
+#include "task.h"
+#include "portable.h"
+#include "semphr.h"
+#include "timers.h"
+
+#include <stdio.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+
+#define CLOCK_FREQUENCY     120000000
+
+#define TIMER_FREQUENCY     4
+#define TIMER_PERIOD        1000/(1.5 * 2 * TIMER_FREQUENCY)
+
+#define ON                  pdTRUE
+#define OFF                 pdFALSE
+
+SemaphoreHandle_t sem_2hz, sem_4hz;
+
+void Task_2Hz(void *pvParameters)
+{
+    BaseType_t led_status = OFF;
+
+    sem_2hz = xSemaphoreCreateCounting(1, 0);
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+
+    while(1)
+    {
+        xSemaphoreTake(sem_2hz, portMAX_DELAY);
+
+        if(led_status)
+        {
+            //Switch off LED
+            led_status = OFF;
+            GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0);
+        }
+
+        else
+        {
+            //Switch on LED
+            led_status = ON;
+            GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
+        }
+    }
+}
+
+void Task_4Hz(void *pvParameters)
+{
+    BaseType_t led_status = OFF;
+
+    sem_4hz = xSemaphoreCreateCounting(1, 0);
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1);
+
+    while(1)
+    {
+        xSemaphoreTake(sem_4hz, portMAX_DELAY);
+
+        if(led_status)
+        {
+            //Switch off LED
+            led_status = OFF;
+            GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0);
+        }
+
+        else
+        {
+            //Switch on LED
+            led_status = ON;
+            GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1);
+        }
+    }
+}
+
+void timer_callback(TimerHandle_t timer)
+{
+    BaseType_t pxHigherPriorityTaskWoken = pdTRUE;
+    static int count = 0;
+
+    //For every two times, give the semaphore to 2hz task. Otherwise, give it  to 4hz task.
+    if(count == 2)
+    {
+        xSemaphoreGiveFromISR(sem_2hz, &pxHigherPriorityTaskWoken);
+        count = -1;
+    }
+    else
+        xSemaphoreGiveFromISR(sem_4hz, &pxHigherPriorityTaskWoken);
+
+    count++;
+}
+
+
+int main(void)
+{
+    uint32_t g_ui32SysClock;
+    BaseType_t timer_id = 0;
+    TimerHandle_t timer;
+
+    // Set the clocking to run directly from the crystal at 120MHz.
+    g_ui32SysClock = MAP_SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+                                             SYSCTL_OSC_MAIN |
+                                             SYSCTL_USE_PLL |
+                                             SYSCTL_CFG_VCO_480), CLOCK_FREQUENCY);
+
+    // Enable the GPIO port that is used for the on-board LED.
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+
+    // Check if the peripheral access is enabled.
+    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPION));
+
+    // Enable the GPIO pin for the LED (PN0).  Set the direction as output, and
+    // enable the GPIO pin for digital function.
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+
+    // Create task for 2Hz Blinking LED
+    xTaskCreate(Task_2Hz, "LED 2Hz", 1000, NULL, 1, NULL );
+
+    // Create task for 4Hz Blinking LED
+    xTaskCreate(Task_4Hz, "LED 4Hz", 1000, NULL, 1, NULL );
+
+    timer = xTimerCreate("4Hz Timer", pdMS_TO_TICKS(TIMER_PERIOD), pdTRUE, (void *)&timer_id, timer_callback);
+    xTimerStart(timer, portMAX_DELAY);
+
+    // Start the scheduler
+    vTaskStartScheduler();
+
+    // Loop forever.
+    while(1){
+    }
+}
